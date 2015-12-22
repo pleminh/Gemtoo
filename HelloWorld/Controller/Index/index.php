633e8a0fac9878db2654c99d8e8227981cb67d9a
Pascal LE MINH <pleminh@absolunet.com>
2015-12-21 23:47:23 -0500

    Initial commit

---
+++ HelloWorld/Controller/Index/index.php
@@ -0,0 +1,32 @@
+<?php
+namespace Gemtoo\HelloWorld\Controller\Index;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param \Magento\Framework\App\Action\Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(\Magento\Framework\App\Action\Context $context,
+                                \Magento\Framework\View\Result\PageFactory $resultPageFactory)
+    {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->getConfig()->getTitle()->prepend(__('Gemtoo HelloWorld'));
+
+        return $resultPage;
+    }
+}
